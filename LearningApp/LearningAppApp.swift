81238f26f270578a66a1d9e57fa36128d54953b9
BananAlmehrej <bananalmehrej@gmail.com>
2024-10-21 09:48:17 +0300

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Banan Almehrej on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
